a93d82b90c27a29b37346f84770de6db1d9a1d69
Kimmo Ahola @ Systementor <kimmo.ahola@systementor.se>
2026-03-02 10:58:31 +0100

    added stuff

---
+++ README.md
@@ -0,0 +1,49 @@
+Skapa funktioner för dessa
+
+## Övning 1
+
+Skapa en variabel temperature.
+
+Om temperature > 25 → skriv "Hot"  
+Annars → skriv "Cold"
+
+---
+
+## Övning 2
+
+Skapa en variabel number.
+
+Om number är jämnt → skriv "Even"  
+Annars → skriv "Odd"
+
+---
+
+## Övning 3
+
+Skapa en variabel salary.
+
+Om salary är:
+
+- > = 50000 → "High"
+    
+- > = 30000 → "Medium"
+    
+- annars → "Low"
+    
+
+---
+
+## Övning 4
+
+Skapa två variabler:
+
+age  
+is_student
+
+Om:
+
+- age < 18 → "Child ticket"
+    
+- age >= 18 och is_student är True → "Student discount"
+    
+- annars → "Full price"
--- first.py
+++ first.py
@@ -1,38 +1,92 @@
-# x = "Kimmo"
+# # x = "Kimmo"
 
-# print(x)
+# # print(x)
 
-# yTest_test123 = "Kimmo"
+# # yTest_test123 = "Kimmo"
+# # age = 34
+
+# # print("Namnet är: ", yTest_test123, "Åldern är: ", age)
+# # x = 12
+
+# # name = "Kimmo"
+# # age = 34
+
+# # z = name + "Ahola"
+
+# # x_2 = 1 # integer
+# # x_3 = 1.0 # float
+# # boolean = True # boolean, False fungerar också
+
+# # result = x_2 + x_3
+# # print(result + boolean)
+# # # kommentar
+# # null = None
+
+# # print(null)
+
+# """
+# kommentar
+# på
+# flera
+# rader
+# """
+
+# name = "kimmo"
 # age = 34
 
-# print("Namnet är: ", yTest_test123, "Åldern är: ", age)
-# x = 12
+# f_string = f"{name} {(age+1)//2}"
+# print(3**6)
 
-# name = "Kimmo"
-# age = 34
+# class Person(): # tabell? ORM -> omvandlar klasser till tabeller
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-# z = name + "Ahola"
+#     def print(self):
+#         print(f"Namnet är: {self.name}. Åldern är: {self.age}")
 
-# x_2 = 1 # integer
-# x_3 = 1.0 # float
-# boolean = True # boolean, False fungerar också
+# kimmo = Person("Kimmo", 34)
 
-# result = x_2 + x_3
-# print(result + boolean)
-# # kommentar
-# null = None
+# # kimmo.print()
 
-# print(null)
+# my_string = "kimmo ahola"
+# my_string = my_string.title()
 
-"""
-kommentar
-på
-flera
-rader
-"""
+# # print(my_string)
 
-name = "kimmo"
-age = 34
+# # Jag vill ha små bokstäver igen
+# my_string = my_string.lower()
 
-f_string = f"{name} {(age+1)//2}"
-print(3**6)
+# my_number = 1
+
+# # Expressions
+
+# expression = (5+4) > 3
+# # print(expression)
+
+# # Jämförelseoperatorer
+
+# # Equals to skrivs ==
+
+# # print("apple" > "Apple")
+
+# # truthy = "typ sant"
+# if 5: # True = 1, False = 0
+#     print("5 is larger than 4?")
+#     print("")
+#     if 5 > 4:
+#         print("")
+# elif 5 > 4:
+#     print("5 is larger than 4!!!!!")
+
+x = "kimmo"
+
+
+def my_capitalize(my_string):
+    x = my_string.upper()
+    print("In my function", x)
+    return x
+
+
+x = my_capitalize(x)  # x = my_string
+print(x)
