8f25020031d3dbf44f10d86bd7a70b43edd4c9f9
Kimmo Ahola @ Systementor <kimmo.ahola@systementor.se>
2026-03-02 10:26:34 +0100

    initial push

---
+++ first.py
@@ -0,0 +1,38 @@
+# x = "Kimmo"
+
+# print(x)
+
+# yTest_test123 = "Kimmo"
+# age = 34
+
+# print("Namnet är: ", yTest_test123, "Åldern är: ", age)
+# x = 12
+
+# name = "Kimmo"
+# age = 34
+
+# z = name + "Ahola"
+
+# x_2 = 1 # integer
+# x_3 = 1.0 # float
+# boolean = True # boolean, False fungerar också
+
+# result = x_2 + x_3
+# print(result + boolean)
+# # kommentar
+# null = None
+
+# print(null)
+
+"""
+kommentar
+på
+flera
+rader
+"""
+
+name = "kimmo"
+age = 34
+
+f_string = f"{name} {(age+1)//2}"
+print(3**6)
